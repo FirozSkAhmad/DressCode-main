12d9cd45a5c3d0eee2490a8e494a5d8645395be1
SarikAnwar01 <sarikanwar007@gmail.com>
2024-02-21 16:30:37 +0545

    image change and cache logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,43 @@
-import React from 'react';
-import Home from './components/home/Home';
+import React from "react";
+import { useEffect } from "react";
+import Home from "./components/home/Home";
 
 const App = () => {
+  useEffect(() => {
+    let registration;
+
+    if ("serviceWorker" in navigator) {
+      const onLoad = () => {
+        navigator.serviceWorker
+          .register("/service-worker.js")
+          .then((reg) => {
+            registration = reg;
+            console.log("Service Worker registered:", registration);
+          })
+          .catch((error) => {
+            console.error("Service Worker registration failed:", error);
+          });
+      };
+
+      window.addEventListener("load", onLoad);
+
+      return () => {
+        // Cleanup function: Remove event listener and unregister service worker
+        window.removeEventListener("load", onLoad);
+        if (registration) {
+          registration.unregister().then(() => {
+            console.log("Service Worker unregistered:", registration);
+          });
+        }
+      };
+    }
+  }, []);
+
   return (
     <>
       <Home></Home>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/home/Choose.jsx
+++ src/components/home/Choose.jsx
@@ -1,113 +1,108 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect } from "react";
 import "./choose.css";
 
-import Fade from 'react-reveal/Fade';
+import Fade from "react-reveal/Fade";
 
 import { useInView } from "framer-motion";
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-
-
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 const Choose = () => {
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    const tl = gsap.timeline();
 
-    useEffect(() => {
-        gsap.registerPlugin(ScrollTrigger);
-        const tl = gsap.timeline();
+    tl.to(".ch_Tl-txt span", 1.8, {
+      y: 0,
+      ease: "power4.out",
+      delay: 0.5,
+      skewY: 0,
 
-        tl.to(".ch_Tl-txt span", 1.8, {
-            y: 0,
-            ease: "power4.out",
-            delay: 0.5,
-            skewY: 0,
+      stagger: {
+        amount: 0.3,
+      },
+      scrollTrigger: {
+        trigger: ".ch_Tl-txt span",
+        start: "top 80%",
+        end: "+=300",
+        scrub: 1,
+        // markers: true,
+        once: true,
+        // pin: true
+      },
+    });
+  }, []);
 
-            stagger: {
-                amount: 0.3
-            },
-            scrollTrigger: {
-                trigger: ".ch_Tl-txt span",
-                start: "top 80%",
-                end: "+=300",
-                scrub: 1,
-                // markers: true,
-                once: true
-                // pin: true
-
-            }
-        })
-
-    }, []);
-
-
-    const ref = useRef(null);
-    const isInView = useInView(ref, { once: true });
-    return (
-        <div className='choose__Wrap'>
-            <div className='choose__Sec'>
-                <div className='choose__Ttl'>
-                    <div className='ch__Ttl-s'>
-                        <div className='ch_Tl-txt'>
-                            <span>Choose your</span>
-                        </div>
-                        <div className='ch_Tl-txt'>
-                            <span>Uniform</span>
-                        </div>
-                    </div>
-                    <div className='ch_ln-wp' ref={ref}>
-                        <span className='ch_ln'
-                            style={{
-                                width: isInView ? "100%" : "64px",
-                                transition: "all 1s cubic-bezier(0.17, 0.55, 0.55, 1) .5s"
-                            }}
-                        ></span>
-                    </div>
-                </div>
-                <div className='choose__Card'>
-                    <Fade bottom duration={1000}>
-                        <div className='ch_Crd'>
-                            <div className='ch_bx-cnt'>
-                                <h4>Uniforms</h4>
-                            </div>
-                            <div className='ch_bx-img'>
-                                <img src="assets/images/uniform.png" alt="uniform" />
-                            </div>
-                        </div>
-                    </Fade>
-
-                    <Fade bottom delay={200} duration={1000}>
-                        <div className='ch_Crd'>
-                            <div className='ch_bx-cnt ch_bx-cnt-bl'>
-                                <h4>School Shoes</h4>
-                            </div>
-                            <div className='ch_bx-img'>
-                                <img src="assets/images/shoes.png" alt="shoes" />
-                            </div>
-                        </div>
-                    </Fade>
-                    <Fade bottom delay={400} duration={1000}>
-                        <div className='ch_Crd'>
-                            <div className='ch_bx-cnt'>
-                                <h4>School Socks</h4>
-                            </div>
-                            <div className='ch_bx-img'>
-                                <img src="assets/images/socks.png" alt="socks" />
-                            </div>
-                        </div>
-                    </Fade>
-                    <Fade bottom delay={600} duration={1000}>
-                        <div className='ch_Crd'>
-                            <div className='ch_bx-cnt ch_bx-cnt-bl'>
-                                <h4>Medical Wear</h4>
-                            </div>
-                            <div className='ch_bx-img'>
-                                <img src="assets/images/medical-wear.png" alt="medical wear" />
-                            </div>
-                        </div>
-                    </Fade>
-                </div>
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+  return (
+    <div className="choose__Wrap">
+      <div className="choose__Sec">
+        <div className="choose__Ttl">
+          <div className="ch__Ttl-s">
+            <div className="ch_Tl-txt">
+              <span>Choose your</span>
             </div>
+            <div className="ch_Tl-txt">
+              <span>Uniform</span>
+            </div>
+          </div>
+          <div className="ch_ln-wp" ref={ref}>
+            <span
+              className="ch_ln"
+              style={{
+                width: isInView ? "100%" : "64px",
+                transition: "all 1s cubic-bezier(0.17, 0.55, 0.55, 1) .5s",
+              }}
+            ></span>
+          </div>
         </div>
-    )
-}
+        <div className="choose__Card">
+          <Fade bottom duration={1000}>
+            <div className="ch_Crd">
+              <div className="ch_bx-cnt">
+                <h4>Uniforms</h4>
+              </div>
+              <div className="ch_bx-img">
+                <img src="assets/images/uniform.png" alt="uniform" />
+              </div>
+            </div>
+          </Fade>
 
-export default Choose
+          <Fade bottom delay={200} duration={1000}>
+            <div className="ch_Crd">
+              <div className="ch_bx-cnt ch_bx-cnt-bl">
+                <h4>Accessories</h4>
+              </div>
+              <div className="ch_bx-img">
+                <img src="assets/images/shoes.png" alt="shoes" />
+              </div>
+            </div>
+          </Fade>
+          <Fade bottom delay={400} duration={1000}>
+            <div className="ch_Crd">
+              <div className="ch_bx-cnt">
+                <h4>Corperate Uniforms</h4>
+              </div>
+              <div className="ch_bx-img">
+                <img src="assets/images/Corporate_uniform.png" alt="socks" />
+              </div>
+            </div>
+          </Fade>
+          <Fade bottom delay={600} duration={1000}>
+            <div className="ch_Crd">
+              <div className="ch_bx-cnt ch_bx-cnt-bl">
+                <h4>Medical Wear</h4>
+              </div>
+              <div className="ch_bx-img">
+                <img src="assets/images/medical-wear.png" alt="medical wear" />
+              </div>
+            </div>
+          </Fade>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Choose;
